300bd57abb7ef4197ff6b5385b5a71ff55c23417
fr3on <ahmed@75bit.com>
2026-04-05 18:35:11 +0200

    fix(dto): fix inconsistency between docs and code in CreateUserCommand

--- dtos/User/CreateUserCommand.php
+++ dtos/User/CreateUserCommand.php
@@ -5,10 +5,18 @@ declare(strict_types=1);
 namespace Dtos\User;
 
 use Quill\Validation\DTO;
+use Quill\Attributes\Required;
+use Quill\Attributes\Email;
+use Quill\Attributes\MinLength;
 
 class CreateUserCommand extends DTO
 {
-    public string $email;
-    public string $name;
-    public string $password;
+    public function __construct(
+        #[Required, Email]
+        public readonly string $email,
+        #[Required, MinLength(2)]
+        public readonly string $name,
+        #[Required, MinLength(8)]
+        public readonly string $password,
+    ) {}
 }
